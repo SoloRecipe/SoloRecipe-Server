589d9c8b128afd241a8fcc856c486664e83f7003
shgur7236 <shgurtns7236@naver.com>
2023-05-22 20:33:46 +0900

    :bug: 매핑 실수

--- src/main/kotlin/com/sr/solorecipe/domain/review/domain/entity/Review.kt
+++ src/main/kotlin/com/sr/solorecipe/domain/review/domain/entity/Review.kt
@@ -1,7 +1,7 @@
 package com.sr.solorecipe.domain.review.domain.entity
 
 import com.fasterxml.jackson.annotation.JsonIgnore
-import com.sr.solorecipe.domain.user.domain.entity.User
+import com.sr.solorecipe.domain.recipe.domain.entity.Recipe
 import com.sr.solorecipe.global.common.entity.BaseIdEntity
 import org.hibernate.annotations.OnDelete
 import org.hibernate.annotations.OnDeleteAction
@@ -17,5 +17,5 @@ data class Review(
     @JsonIgnore
     @ManyToOne(fetch = FetchType.LAZY)
     @OnDelete(action = OnDeleteAction.CASCADE)
-    val user: User
+    val recipe: Recipe
 ):BaseIdEntity()
