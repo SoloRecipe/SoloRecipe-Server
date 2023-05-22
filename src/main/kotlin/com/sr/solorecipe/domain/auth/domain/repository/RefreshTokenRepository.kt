9e746bb5dcacee2a2c1da98d8915a1081bf34c65
shgur7236 <shgurtns7236@naver.com>
2023-05-22 20:58:23 +0900

    :sparkles: Create RefreshToken Repository

---
+++ src/main/kotlin/com/sr/solorecipe/domain/auth/domain/repository/RefreshTokenRepository.kt
@@ -0,0 +1,7 @@
+package com.sr.solorecipe.domain.auth.domain.repository
+
+import com.sr.solorecipe.domain.auth.domain.entity.RefreshToken
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface RefreshTokenRepository: JpaRepository<RefreshToken, Long> {
+}
