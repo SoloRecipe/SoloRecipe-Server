cea61ee58f77041344f3b6299598413f9cca20e2
shgur7236 <shgurtns7236@naver.com>
2023-05-22 19:37:19 +0900

    :bug: id -> idx

--- src/main/kotlin/com/sr/solorecipe/global/common/entity/BaseIdEntity.kt
+++ src/main/kotlin/com/sr/solorecipe/global/common/entity/BaseIdEntity.kt
@@ -9,5 +9,5 @@ import javax.persistence.MappedSuperclass
 abstract class BaseIdEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    var id: Long = 0
+    var idx: Long = 0
 }
