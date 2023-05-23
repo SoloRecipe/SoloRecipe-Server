598cf278ac4dd020666177cfe95d10abdc479ce9
shgur7236 <shgurtns7236@naver.com>
2023-05-23 23:45:54 +0900

    :sparkles: 초기 값에 암호화 시키기

--- src/main/kotlin/com/sr/solorecipe/global/security/jwt/properties/JwtProperties.kt
+++ src/main/kotlin/com/sr/solorecipe/global/security/jwt/properties/JwtProperties.kt
@@ -1,11 +1,22 @@
 package com.sr.solorecipe.global.security.jwt.properties
 
+import io.jsonwebtoken.security.Keys
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConstructorBinding
+import java.nio.charset.StandardCharsets
+import java.security.Key
 
 @ConstructorBinding
 @ConfigurationProperties(prefix = "jwt")
 class JwtProperties(
-    val accessToken: String,
-    val refreshToken: String
-)
+    accessToken: String,
+    refreshToken: String
+){
+    val accessToken: Key
+    val refreshToken: Key
+
+    init {
+        this.accessToken = Keys.hmacShaKeyFor(accessToken.toByteArray(StandardCharsets.UTF_8))
+        this.refreshToken = Keys.hmacShaKeyFor(refreshToken.toByteArray(StandardCharsets.UTF_8))
+    }
+}
