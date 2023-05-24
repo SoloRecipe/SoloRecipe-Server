7e7f450258b74d4539e70d6e71a8d74837582b9f
shgur7236 <shgurtns7236@naver.com>
2023-05-24 19:49:29 +0900

    :sparkles: passwordEncoder 빈 등록

---
+++ src/main/kotlin/com/sr/solorecipe/global/security/config/PasswordEncodeConfig.kt
@@ -0,0 +1,12 @@
+package com.sr.solorecipe.global.security.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+
+@Configuration
+class PasswordEncodeConfig {
+    @Bean
+    fun passwordEncoder(): PasswordEncoder? = BCryptPasswordEncoder()
+}
