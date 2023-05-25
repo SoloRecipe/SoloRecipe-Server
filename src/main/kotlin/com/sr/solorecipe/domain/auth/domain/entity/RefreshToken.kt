9a6c784ca2110a8a703331da8267223cf1d094be
shgur7236 <shgurtns7236@naver.com>
2023-05-25 15:15:05 +0900

    :sparkles: @Indexed

--- src/main/kotlin/com/sr/solorecipe/domain/auth/domain/entity/RefreshToken.kt
+++ src/main/kotlin/com/sr/solorecipe/domain/auth/domain/entity/RefreshToken.kt
@@ -2,10 +2,13 @@ package com.sr.solorecipe.domain.auth.domain.entity
 
 import org.springframework.data.annotation.Id
 import org.springframework.data.redis.core.RedisHash
+import org.springframework.data.redis.core.index.Indexed
 
 @RedisHash(value = "refresh_token", timeToLive = 60 * 60 * 24 * 7)
 data class RefreshToken(
     @Id
+    @Indexed
     val userIdx: Long,
+    @Indexed
     val token: String
 )
