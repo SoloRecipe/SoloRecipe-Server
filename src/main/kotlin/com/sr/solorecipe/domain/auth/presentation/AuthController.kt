b4ba66ba5cdf3576f3c5a7d106dc5f1015051162
shgur7236 <shgurtns7236@naver.com>
2023-05-25 15:15:30 +0900

    :sparkles: 재발급 Controller

--- src/main/kotlin/com/sr/solorecipe/domain/auth/presentation/AuthController.kt
+++ src/main/kotlin/com/sr/solorecipe/domain/auth/presentation/AuthController.kt
@@ -3,13 +3,16 @@ package com.sr.solorecipe.domain.auth.presentation
 import com.sr.solorecipe.domain.auth.presentation.data.request.LoginRequest
 import com.sr.solorecipe.domain.auth.presentation.data.request.SignUpRequest
 import com.sr.solorecipe.domain.auth.presentation.data.response.TokenResponse
+import com.sr.solorecipe.domain.auth.service.GetNewRefreshTokenService
 import com.sr.solorecipe.domain.auth.service.LoginService
 import com.sr.solorecipe.domain.auth.service.SignUpService
 import com.sr.solorecipe.domain.auth.util.AuthConverter
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -19,6 +22,7 @@ class AuthController(
     private val authConverter: AuthConverter,
     private val signUpService: SignUpService,
     private val loginService: LoginService,
+    private val getNewRefreshTokenService: GetNewRefreshTokenService,
 ) {
 
     @PostMapping("/register")
@@ -32,4 +36,7 @@ class AuthController(
         authConverter.toDto(loginRequest)
             .let { ResponseEntity.ok(authConverter.toResponse(loginService.login(it))) }
 
+    @PatchMapping
+    fun getNewRefreshToken(@RequestHeader("refresh-token") refreshToken: String): ResponseEntity<TokenResponse> =
+        ResponseEntity.ok().body(authConverter.toResponse(getNewRefreshTokenService.get(refreshToken)))
 }
