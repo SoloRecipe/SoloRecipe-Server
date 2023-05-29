2bea0e5aeb54b6296d9bc0cb3e42b81c042b478b
shgur7236 <shgurtns7236@naver.com>
2023-05-29 16:07:50 +0900

    :bug:  JPA메서드를 통해 정렬 후 pagination

--- src/main/kotlin/com/sr/solorecipe/domain/recipe/service/impl/GetRecipeListSortedByRecipeViewsServiceImpl.kt
+++ src/main/kotlin/com/sr/solorecipe/domain/recipe/service/impl/GetRecipeListSortedByRecipeViewsServiceImpl.kt
@@ -6,7 +6,6 @@ import com.sr.solorecipe.domain.recipe.service.GetRecipeListSortedByRecipeViewsS
 import com.sr.solorecipe.domain.recipe.util.RecipeConverter
 import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
-import kotlin.streams.toList
 
 @Service
 class GetRecipeListSortedByRecipeViewsServiceImpl(
@@ -14,10 +13,8 @@ class GetRecipeListSortedByRecipeViewsServiceImpl(
     private val recipeRepository: RecipeRepository,
 ): GetRecipeListSortedByRecipeViewsService {
     override fun getRecipeList(pageable: Pageable): RecipeListDto {
-        val sortedByRecipeViewsListDto = recipeRepository.findAll(pageable).stream()
+        val sortedByRecipeViewsListDto = recipeRepository.findAllByOrderByRecipeViewsViewsDesc(pageable)
             .map { recipe -> recipeConverter.toDto(recipe) }
-            .sorted(compareByDescending { it.views })
-            .toList()
 
         return recipeConverter.toDto(pageable, sortedByRecipeViewsListDto)
     }
