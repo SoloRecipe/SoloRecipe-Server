44b39c8d7563a7910c8d8caddef72e4322948687
shgur7236 <shgurtns7236@naver.com>
2023-05-31 12:13:24 +0900

    :sparkles: recipe detail 에서 사용하는 메서드 구현

--- src/main/kotlin/com/sr/solorecipe/domain/recipe/util/RecipeConverter.kt
+++ src/main/kotlin/com/sr/solorecipe/domain/recipe/util/RecipeConverter.kt
@@ -1,15 +1,24 @@
 package com.sr.solorecipe.domain.recipe.util
 
 import com.sr.solorecipe.domain.recipe.domain.entity.Recipe
+import com.sr.solorecipe.domain.recipe.domain.entity.RecipeProcess
+import com.sr.solorecipe.domain.recipe.presentation.data.dto.RecipeDetailDto
 import com.sr.solorecipe.domain.recipe.presentation.data.dto.RecipeDto
 import com.sr.solorecipe.domain.recipe.presentation.data.dto.RecipeListDto
+import com.sr.solorecipe.domain.recipe.presentation.data.dto.RecipeProcessDto
+import com.sr.solorecipe.domain.recipe.presentation.data.response.RecipeDetailResponse
 import com.sr.solorecipe.domain.recipe.presentation.data.response.RecipeListResponse
+import com.sr.solorecipe.domain.recipe.presentation.data.response.RecipeProcessResponse
 import com.sr.solorecipe.domain.recipe.presentation.data.response.RecipeResponse
+import com.sr.solorecipe.domain.review.domain.presentation.dto.ReviewDto
+import com.sr.solorecipe.domain.review.domain.presentation.response.ReviewResponse
 import org.springframework.data.domain.Pageable
 
 interface RecipeConverter {
     fun toDto(recipe: Recipe): RecipeDto
     fun toDto(pageable: Pageable, recipeDto: List<RecipeDto>): RecipeListDto
+    fun toDto(recipe: Recipe, recipeProcessDto: List<RecipeProcessDto>, reviewDto: List<ReviewDto>): RecipeDetailDto
     fun toResponse(recipeDto: RecipeDto): RecipeResponse
     fun toResponse(pageable: Pageable, recipeListResponse: List<RecipeResponse>): RecipeListResponse
+    fun toResponse(recipeDetailDto: RecipeDetailDto, recipeProcessResponse: List<RecipeProcessResponse>, reviewResponse: List<ReviewResponse>): RecipeDetailResponse
 }
