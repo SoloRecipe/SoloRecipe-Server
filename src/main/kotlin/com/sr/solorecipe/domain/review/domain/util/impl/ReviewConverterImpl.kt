474c7ced5e87c1ec703bdeba26db2c683f5aa805
shgur7236 <shgurtns7236@naver.com>
2023-05-31 12:13:44 +0900

    :sparkles: recipe detail 에서 사용하는 메서드 구현

---
+++ src/main/kotlin/com/sr/solorecipe/domain/review/domain/util/impl/ReviewConverterImpl.kt
@@ -0,0 +1,26 @@
+package com.sr.solorecipe.domain.review.domain.util.impl
+
+import com.sr.solorecipe.domain.review.domain.entity.Review
+import com.sr.solorecipe.domain.review.domain.presentation.dto.ReviewDto
+import com.sr.solorecipe.domain.review.domain.presentation.response.ReviewResponse
+import com.sr.solorecipe.domain.review.domain.util.ReviewConverter
+import org.springframework.stereotype.Component
+
+@Component
+class ReviewConverterImpl(
+
+): ReviewConverter {
+    override fun toDto(review: Review): ReviewDto =
+        ReviewDto(
+            userIdx = review.recipe.user.idx,
+            userName = review.recipe.user.nickname,
+            content = review.content
+        )
+
+    override fun toResponse(reviewDto: ReviewDto): ReviewResponse =
+        ReviewResponse(
+            userIdx = reviewDto.userIdx,
+            userName = reviewDto.userName,
+            content = reviewDto.content
+        )
+}
