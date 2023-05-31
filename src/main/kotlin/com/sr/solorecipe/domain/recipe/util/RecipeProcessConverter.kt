a0a461a4720ed7616d57a7c499013aa4201e83c8
shgur7236 <shgurtns7236@naver.com>
2023-05-31 18:01:42 +0900

    :sparkles: converter 구현

--- src/main/kotlin/com/sr/solorecipe/domain/recipe/util/RecipeProcessConverter.kt
+++ src/main/kotlin/com/sr/solorecipe/domain/recipe/util/RecipeProcessConverter.kt
@@ -1,11 +1,14 @@
 package com.sr.solorecipe.domain.recipe.util
 
 import com.sr.solorecipe.domain.recipe.domain.entity.RecipeProcess
+import com.sr.solorecipe.domain.recipe.presentation.data.dto.ModifyRecipeProcessDto
 import com.sr.solorecipe.domain.recipe.presentation.data.dto.RecipeProcessDto
+import com.sr.solorecipe.domain.recipe.presentation.data.request.ModifyRecipeProcessRequest
 import com.sr.solorecipe.domain.recipe.presentation.data.response.RecipeProcessResponse
 
 interface RecipeProcessConverter {
     fun toDto(recipeProcess: RecipeProcess): RecipeProcessDto
+    fun toDto(modifyRecipeProcessRequest: ModifyRecipeProcessRequest): ModifyRecipeProcessDto
     fun toResponse(recipeProcessDto: RecipeProcessDto): RecipeProcessResponse
 
 }
--- src/main/kotlin/com/sr/solorecipe/domain/recipe/util/impl/RecipeProcessConverterImpl.kt
+++ src/main/kotlin/com/sr/solorecipe/domain/recipe/util/impl/RecipeProcessConverterImpl.kt
@@ -1,7 +1,9 @@
 package com.sr.solorecipe.domain.recipe.util.impl
 
 import com.sr.solorecipe.domain.recipe.domain.entity.RecipeProcess
+import com.sr.solorecipe.domain.recipe.presentation.data.dto.ModifyRecipeProcessDto
 import com.sr.solorecipe.domain.recipe.presentation.data.dto.RecipeProcessDto
+import com.sr.solorecipe.domain.recipe.presentation.data.request.ModifyRecipeProcessRequest
 import com.sr.solorecipe.domain.recipe.presentation.data.response.RecipeProcessResponse
 import com.sr.solorecipe.domain.recipe.util.RecipeProcessConverter
 import org.springframework.stereotype.Component
@@ -15,6 +17,12 @@ class RecipeProcessConverterImpl(
             idx = recipeProcess.idx,
             description = recipeProcess.description,
             image = recipeProcess.image
+        )
+
+    override fun toDto(modifyRecipeProcessRequest: ModifyRecipeProcessRequest): ModifyRecipeProcessDto =
+        ModifyRecipeProcessDto(
+            description = modifyRecipeProcessRequest.description,
+            image = modifyRecipeProcessRequest.image
         )
 
     override fun toResponse(recipeProcessDto: RecipeProcessDto): RecipeProcessResponse =
