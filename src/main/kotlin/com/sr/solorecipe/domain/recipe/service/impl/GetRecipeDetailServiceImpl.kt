1249d81c122e665ce0e24ded49974f1463aea4db
shgur7236 <shgurtns7236@naver.com>
2023-06-02 18:10:43 +0900

    :sparkles: event 생성

--- src/main/kotlin/com/sr/solorecipe/domain/recipe/service/impl/GetRecipeDetailServiceImpl.kt
+++ src/main/kotlin/com/sr/solorecipe/domain/recipe/service/impl/GetRecipeDetailServiceImpl.kt
@@ -2,6 +2,7 @@ package com.sr.solorecipe.domain.recipe.service.impl;
 
 import com.sr.solorecipe.domain.recipe.domain.repository.RecipeProcessRepository
 import com.sr.solorecipe.domain.recipe.domain.repository.RecipeRepository
+import com.sr.solorecipe.domain.recipe.event.SawRecipeDetailEvent
 import com.sr.solorecipe.domain.recipe.exception.RecipeNotFoundException
 import com.sr.solorecipe.domain.recipe.presentation.data.dto.RecipeDetailDto
 import com.sr.solorecipe.domain.recipe.service.GetRecipeDetailService
@@ -9,9 +10,12 @@ import com.sr.solorecipe.domain.recipe.util.RecipeConverter
 import com.sr.solorecipe.domain.recipe.util.RecipeProcessConverter
 import com.sr.solorecipe.domain.review.domain.repository.ReviewRepository
 import com.sr.solorecipe.domain.review.domain.util.ReviewConverter
+import org.springframework.context.ApplicationEventPublisher
 import org.springframework.stereotype.Service;
+import javax.transaction.Transactional
 
 @Service
+@Transactional(rollbackOn = [Exception::class])
 class GetRecipeDetailServiceImpl(
     private val recipeConverter: RecipeConverter,
     private val recipeRepository: RecipeRepository,
@@ -19,6 +23,7 @@ class GetRecipeDetailServiceImpl(
     private val recipeProcessConverter: RecipeProcessConverter,
     private val reviewRepository: ReviewRepository,
     private val reviewConverter: ReviewConverter,
+    private val publisher: ApplicationEventPublisher,
 ): GetRecipeDetailService {
     override fun getRecipeDetail(idx: Long): RecipeDetailDto {
         val recipe = recipeRepository.findById(idx)
@@ -27,6 +32,8 @@ class GetRecipeDetailServiceImpl(
             .map { recipeProcessConverter.toDto(it) }
         val reviewDto = reviewRepository.findAllByRecipeIdx(idx)
             .map { reviewConverter.toDto(it) }
+
+        publisher.publishEvent(SawRecipeDetailEvent(recipe.recipeViews))
 
         return recipeConverter.toDto(recipe, recipeProcessDto, reviewDto)
     }
