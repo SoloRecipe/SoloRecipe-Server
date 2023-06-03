2e12d223f3486d8a1730b8ca6eece76d2fce5b69
shgur7236 <shgurtns7236@naver.com>
2023-06-03 19:46:03 +0900

    :sparkles: 메서드 추가

--- src/main/kotlin/com/sr/solorecipe/domain/recipe/domain/repository/RecipeRepository.kt
+++ src/main/kotlin/com/sr/solorecipe/domain/recipe/domain/repository/RecipeRepository.kt
@@ -1,11 +1,11 @@
 package com.sr.solorecipe.domain.recipe.domain.repository
 
 import com.sr.solorecipe.domain.recipe.domain.entity.Recipe
-import org.springframework.data.domain.Page
+import com.sr.solorecipe.domain.user.domain.entity.User
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface RecipeRepository: JpaRepository<Recipe, Long> {
     fun findAllByOrderByRecipeViewsViewsDesc(pageable: Pageable): List<Recipe>
-
+    fun findAllByUser(user: User): List<Recipe>
 }
