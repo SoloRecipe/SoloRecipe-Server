e0ad8f60963e90e36e40886ad4faf26c52f8c679
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-06-06 17:05:07 +0900

    :sparkles: Dto 생성

--- src/main/kotlin/com/sr/solorecipe/domain/user/presentation/UserController.kt
+++ src/main/kotlin/com/sr/solorecipe/domain/user/presentation/UserController.kt
@@ -1,10 +1,13 @@
 package com.sr.solorecipe.domain.user.presentation
 
+import com.sr.solorecipe.domain.user.presentation.request.UpdateUserNameRequest
 import com.sr.solorecipe.domain.user.presentation.response.UserInfoResponse
 import com.sr.solorecipe.domain.user.service.GetUserInfoService
 import com.sr.solorecipe.domain.user.util.UserConverter
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -24,5 +27,8 @@ class UserController(
 
         return ResponseEntity.ok(userConverter.toResponse(userInfoDto.name, likeRecipe, myRecipe))
     }
-
+    @PatchMapping
+    fun updateUserName(@RequestBody request: UpdateUserNameRequest): ResponseEntity<Void> {
+        return ResponseEntity.noContent().build()
+    }
 }
---
+++ src/main/kotlin/com/sr/solorecipe/domain/user/presentation/request/UpdateUserNameRequest.kt
@@ -0,0 +1,5 @@
+package com.sr.solorecipe.domain.user.presentation.request
+
+data class UpdateUserNameRequest(
+        val name: String
+)
