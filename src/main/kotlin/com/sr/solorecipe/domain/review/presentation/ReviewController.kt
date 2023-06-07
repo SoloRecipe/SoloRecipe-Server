7429cc52bc2619f2093d74028cbc2276e71aa0d6
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-06-07 23:07:24 +0900

    :sparkles: 서비스 구현

--- src/main/kotlin/com/sr/solorecipe/domain/review/presentation/ReviewController.kt
+++ src/main/kotlin/com/sr/solorecipe/domain/review/presentation/ReviewController.kt
@@ -2,6 +2,7 @@ package com.sr.solorecipe.domain.review.presentation
 
 import com.sr.solorecipe.domain.review.presentation.request.UpdateReviewRequest
 import com.sr.solorecipe.domain.review.presentation.request.WriteReviewRequest
+import com.sr.solorecipe.domain.review.service.RemoveReviewService
 import com.sr.solorecipe.domain.review.service.UpdateReviewService
 import com.sr.solorecipe.domain.review.service.WriteReviewService
 import com.sr.solorecipe.domain.review.util.ReviewConverter
@@ -14,7 +15,8 @@ import org.springframework.web.bind.annotation.*
 class ReviewController(
     private val reviewConverter: ReviewConverter,
     private val writeReviewService: WriteReviewService,
-    private val updateReviewService: UpdateReviewService
+    private val updateReviewService: UpdateReviewService,
+    private val removeReviewService: RemoveReviewService
 ) {
     @PostMapping("/{recipe-idx}")
     fun writeReview(@PathVariable("recipe-idx")idx: Long, @RequestBody request: WriteReviewRequest): ResponseEntity<Void> {
@@ -24,6 +26,11 @@ class ReviewController(
     @PatchMapping("/{review-idx}")
     fun updateReview(@PathVariable("review-idx")idx: Long, @RequestBody request: UpdateReviewRequest): ResponseEntity<Void> {
         updateReviewService.update(reviewConverter.toDto(request, idx))
+        return ResponseEntity.noContent().build()
+    }
+    @DeleteMapping("/{review-idx}")
+    fun removeReview(@PathVariable("review-idx")idx: Long): ResponseEntity<Void> {
+        removeReviewService.remove(idx)
         return ResponseEntity.noContent().build()
     }
 }
