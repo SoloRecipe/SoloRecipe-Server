edd1a5c503028ae984685c809275c8d39a87e67c
shgur7236 <shgurtns7236@naver.com>
2023-06-07 15:19:13 +0900

    :sparkles: Write Review Converter 구현

--- src/main/kotlin/com/sr/solorecipe/domain/review/util/ReviewConverter.kt
+++ src/main/kotlin/com/sr/solorecipe/domain/review/util/ReviewConverter.kt
@@ -1,15 +1,10 @@
 package com.sr.solorecipe.domain.review.util
 
-import com.sr.solorecipe.domain.recipe.domain.entity.Recipe
 import com.sr.solorecipe.domain.review.domain.entity.Review
 import com.sr.solorecipe.domain.review.presentation.dto.ReviewDto
-import com.sr.solorecipe.domain.review.presentation.dto.WriteReviewDto
-import com.sr.solorecipe.domain.review.presentation.request.WriteReviewRequest
 import com.sr.solorecipe.domain.review.presentation.response.ReviewResponse
 
 interface ReviewConverter {
     fun toDto(review: Review): ReviewDto
     fun toResponse(reviewDto: ReviewDto): ReviewResponse
-    fun toDto(request: WriteReviewRequest): WriteReviewDto
-    fun toEntity(foundRecipe: Recipe, writeReviewDto: WriteReviewDto): Review
 }
